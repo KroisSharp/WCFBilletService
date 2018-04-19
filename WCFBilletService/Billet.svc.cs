599e59c2c9d763fd265ccdfaaf2ed3e652cf6f4c
kroissharp <mikx0067@edu.easj.dk>
2018-04-19 17:29:04 +0200

    Add project files.

---
+++ WCFBilletService/Billet.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using BilletLib;
+
+namespace WCFBilletService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Billet : IBillet
+    {
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //mine forskellige objs 
+        private Bil BilStoreBælt = new Bil(false);
+        private MC MCStoreBælt = new MC(false);
+        private Bil BilØresund = new Bil(true);
+        private MC MCØresund = new MC(true);
+
+        public int BilPrisUdenRabatStorebælt()
+        {
+            return BilStoreBælt.Pris();
+        }
+
+
+        public int MCPrisUdenRabatStorebælt()
+        {
+            return MCStoreBælt.Pris();
+        }
+
+
+        public int BilPrisUdenRabatØresund()
+        {
+            return BilØresund.Pris();
+        }
+
+
+        public int MCPrisUdenRabatØresund()
+        {
+            return MCØresund.Pris();
+        }
+    }
+}
---
+++ WCFBilletServiceTests/BilletTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFBilletService;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFBilletService.Tests
+{
+    [TestClass()]
+    public class BilletTests
+    {
+        [TestMethod()]
+        public void BilPrisUdenRabatStorebæltTest()
+        {
+            //arrange
+            Billet minbillet = new Billet();
+            //act
+            int minint = minbillet.BilPrisUdenRabatStorebælt();
+            //assert
+            Assert.AreEqual(240, minint);
+        }
+
+        [TestMethod()]
+        public void MCPrisUdenRabatStorebæltTest()
+        {
+            //arrange
+            Billet minbillet = new Billet();
+            //act
+            int minint = minbillet.MCPrisUdenRabatStorebælt();
+            //assert
+            Assert.AreEqual(125, minint);
+        }
+
+        [TestMethod()]
+        public void BilPrisUdenRabatØresundTest()
+        {
+            //arrange
+            Billet minbillet = new Billet();
+            //act
+            int minint = minbillet.BilPrisUdenRabatØresund();
+            //assert
+            Assert.AreEqual(240, minint);
+        }
+
+        [TestMethod()]
+        public void MCPrisUdenRabatØresundTest()
+        {
+            //arrange
+            Billet minbillet = new Billet();
+            //act
+            int minint = minbillet.MCPrisUdenRabatØresund();
+            //assert
+            Assert.AreEqual(125, minint);
+        }
+    }
+}
